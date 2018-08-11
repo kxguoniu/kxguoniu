82aaefa21d3ab5627f1fabb8fe4e56d8f6e6d896
kxguoniu <13213790823@163.com>
2018-08-11 20:24:57 +0800

    1

--- Scrapy/tree.py
+++ Scrapy/tree.py
@@ -1,3 +1,5 @@
+import csv
+
 class Node(object):
     def __init__(self, data = {}):
         self.name = data.get('name')                #姓名
@@ -9,64 +11,71 @@ class Node(object):
         self.number = data.get('number')            #工号
         self.position = data.get('position')        #职位
         self.superior = data.get('superior')        #上级
-        self.subordinate = data.get('subordinate')  #下级
+        self.subordinate = data.get('subordinate', [])  #下级
         self.branch = data.get('branch')            #部门
 
-    def name_change(name):
+    def name_change(self, name):
         self.name = name
-    def sex_change(sex):
+    def sex_change(self, sex):
         self.sex = sex
-    def mobile_change(mobile):
+    def mobile_change(self, mobile):
         self.mobile = mobile
-    def address_change(address):
+    def address_change(self, address):
         self.address = address
-    def phone_change(phone):
+    def phone_change(self, phone):
         self.phone = phone
-    def mail_change(mail):
+    def mail_change(self, mail):
         self.mail = mail
-    def number_change(number):
+    def number_change(self, number):
         self.number = number
-    def position_cahnge(position):
+    def position_cahnge(self, position):
         self.position = position
-    def subordinate_add(subordinate):
-        self.subordinate.add(subordinate)
-    def subordinate_delete(subordinate):
-        self.subordinate.remove(subordinate)
-    def superior_add(superior):
+    def superior_add(self, superior):
         self.superior.add(superior)
-    def superior_delete(superior):
+    def superior_delete(self, superior):
         self.superior.remove(superior)
-    def branch_add(branch):
+    def subordinate_add(self, subordinate):
+        self.subordinate.append(subordinate)
+    def subordinate_delete(self, subordinate):
+        self.subordinate.remove(subordinate)
+    def branch_add(self, branch):
         self.branch.add(branch)
-    def branch_delete(branch):
+    def branch_delete(self, branch):
         self.branch.remove(branch)
 
 
 class Tree(object):
     def __init__(self):
-        self.head = None
-
-    def isEmpty(self):
-        return (self.length == 0)
+        self.head = Node()
 
     def append(self, data, name=None):
-        if not isinstance(dataOrNode, Node):
+        if not isinstance(data, Node):
             return False
 
         if name == None:
+            return 'append(data, name=None) 缺少name'
+
+        if name == 'admin':
+            data.superior = 'admin'
             self.head = data
-            return True
+            return '添加头结点成功'
         
-        node = self.head
-        result = deep_find(node, name)
+        result = self.deep_find(name)
         if not result:
             return 'name 节点不存在'
-        result.subordinate.append(dataOrNode)
-        return True
+        if not data.superior:
+            data.superior = name
+        else:
+            if data.superior != name:
+                return '上级不符合'
+        result.subordinate.append(data)
+        return '添加成功'
 
-            
 
-    def deep_find(self, node, value, path=[]):
+    def deep_find(self, name):
+        node = self.head
+        if node.name == name:
+            return node
         stack_list = []
         visited = []
         stack_list.append(node)
@@ -75,13 +84,84 @@ class Tree(object):
             x = stack_list[-1]
             for w in x.subordinate:
                 if not w in visited:
-                    print(w.name)
-                    if w.name == value:
+                    if w.name == name:
                         return w
                     visited.append(w)
                     stack_list.append(w)
                     break
             if stack_list[-1] == x:
                 stack_list.pop()
-
         return False
+
+
+    def save(self):
+        stack_list = [self.head]
+        with open('database.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            fieldnames = ['name', 'sex', 'mobile', 'address', 'phone', 'mail', 'number', 'position', 'superior', 'subordinate', 'branch']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            while len(stack_list) > 0:
+                node = stack_list[0]
+                lists1 = [i.name for i in node.subordinate]
+                lists2 = [i for i in node.subordinate]
+                data = {'name': node.name,
+                        'sex': node.sex,
+                        'mobile': node.mobile,
+                        'address': node.address,
+                        'phone': node.phone,
+                        'mail': node.mail,
+                        'number': node.mail,
+                        'position': node.position,
+                        'superior': node.superior,
+                        'subordinate': lists1,
+                        'branch': node.branch}
+                writer.writerow(data)
+                stack_list.extend(lists2)
+                stack_list.pop(0)
+
+
+    def read(self):
+        t = Tree()
+        with open('database.csv', 'r', encoding='utf-8') as csvfile:
+            csvfile.readline()
+            reader = csv.reader(csvfile)
+            print (reader)
+            for row in reader:
+                data = {'name': row[0],
+                        'sex': row[1],
+                        'mobile': row[2],
+                        'address': row[3],
+                        'phone': row[4],
+                        'mail': row[5],
+                        'number': row[6],
+                        'position': row[7],
+                        'superior': row[8],
+                        'subordinate': [],
+                        'branch': row[10]}
+                t.append(Node(data), data['superior'])
+        return t
+
+
+def main():
+    a = {'name':'a', 'sex':20}
+    b = {'name':'b', 'sex':20}
+    c = {'name':'c', 'sex':20}
+    d = {'name':'d', 'sex':20}
+    e = {'name':'e', 'sex':20}
+    f = {'name':'f', 'sex':20}
+    g = {'name':'g', 'sex':20}
+    h = {'name':'h', 'sex':20}
+
+    t = Tree()
+    t.append(Node(a), 'admin')
+    t.append(Node(b), 'a')
+    t.append(Node(c), 'a')
+    t.append(Node(d), 'a')
+    t.append(Node(e), 'b')
+    t.append(Node(f), 'b')
+    t.append(Node(g), 'c')
+    t.append(Node(h), 'd')
+    t.save()
+
+t = Tree()
+t = t.read()
